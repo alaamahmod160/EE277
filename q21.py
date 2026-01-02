5279b439b797a141d337d12ff9fb818c41884cc4
alaamahmod160 <elarabea003@gmail.com>
2026-01-03 00:56:05 +0200

    Add files via upload

---
+++ q21.py
@@ -0,0 +1,15 @@
+﻿# Bayesian Inference Example - numeric version
+
+# Given numbers
+P_D = 0.02
+P_notD = 0.98
+sensitivity = 0.95
+specificity = 0.90
+
+# False positive rate
+P_pos_given_notD = 1 - specificity
+
+# Posterior probability
+P_D_given_pos = (sensitivity * P_D) / (sensitivity*P_D + P_pos_given_notD * P_notD)
+
+print("Posterior probability P(D|Positive) ≈", round(P_D_given_pos, 3))
